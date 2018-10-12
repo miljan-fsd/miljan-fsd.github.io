82740318312539cfd54ed252caed4561eb99bcee
Miljan Djordjevic <miljan.fsd@gmail.com>
2018-10-12 21:04:04 +0200

    Add Particles

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import Particles from 'react-particles-js'
+
 import './App.css'
-import profileImage from '../images/miljandjordjevic.jpeg'
+import profileImage from '../images/miljandjordjevic.jpg'
+
+const particlesParams = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   static defaultProps = {
@@ -13,84 +27,63 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <img src={profileImage} className="img" alt={this.props.name} />
+        <Particles params={particlesParams} className="particles" />
+        <div className="profile">
+          <img src={profileImage} className="img" alt={this.props.name} />
 
-        <h1 className="name">{this.props.name}</h1>
+          <h1 className="name">{this.props.name}</h1>
 
-        <p className="title">{this.props.title}</p>
+          <p className="title">{this.props.title}</p>
 
-        <div id="social">
-          <a
-            href="https://rs.linkedin.com/in/miljandjordjevic"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-linkedin fa-3x social-item linkedin"
-              title="Linkedin"
-              aria-hidden="true"
-            />
-          </a>
+          <div id="social">
+            <a
+              href="https://rs.linkedin.com/in/miljandjordjevic"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                className="fab fa-linkedin-in fa-2x social-item linkedin"
+                title="Linkedin"
+                aria-hidden="true"
+              />
+            </a>
 
-          <a
-            href="https://github.com/miljan-fsd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-github fa-3x social-item github"
-              title="GitHub"
-              aria-hidden="true"
-            />
-          </a>
+            <a
+              href="https://github.com/miljan-fsd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                className="fab fa-github fa-2x social-item github"
+                title="GitHub"
+                aria-hidden="true"
+              />
+            </a>
 
-          <a
-            href="https://www.freecodecamp.com/miljan-fsd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-free-code-camp fa-3x social-item freecodecamp"
-              title="FreeCodeCamp"
-              aria-hidden="true"
-            />
-          </a>
+            <a
+              href="https://twitter.com/miljan_fsd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                className="fab fa-twitter fa-2x social-item twitter"
+                title="Twitter"
+                aria-hidden="true"
+              />
+            </a>
 
-          <a
-            href="https://www.facebook.com/miljandjordjevic87"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-facebook-square fa-3x social-item facebook"
-              title="Facebook"
-              aria-hidden="true"
-            />
-          </a>
-
-          <a
-            href="https://plus.google.com/+MiljanDjordjevic87"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-google-plus-square fa-3x social-item google-plus"
-              title="Google+"
-              aria-hidden="true"
-            />
-          </a>
-
-          <a
-            href="https://twitter.com/miljan_fsd"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i
-              className="fab fa-twitter-square fa-3x social-item twitter"
-              title="Twitter"
-              aria-hidden="true"
-            />
-          </a>
+            <a
+              href="https://www.freecodecamp.com/miljan-fsd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i
+                className="fab fa-free-code-camp fa-2x social-item freecodecamp"
+                title="FreeCodeCamp"
+                aria-hidden="true"
+              />
+            </a>
+          </div>
         </div>
       </div>
     )
